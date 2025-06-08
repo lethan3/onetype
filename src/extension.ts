d1f790a8128d2f86d504e0c17969be4b7b3a7f2b
lethan3 <ethanlnow@gmail.com>
2025-06-07 22:36:15 -0700

    add leaves and a bunch of things

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { watch } from 'fs';
 import { userInfo } from 'os';
 import * as vscode from 'vscode';
 import * as vsls from 'vsls';
@@ -6,9 +7,10 @@ let inSession = false;
 let host: string | null = null;
 let editor: string | null = null;
 let users: string[] = [];
+let idToUsername: Map<number, string> = new Map();
 let requests: string[] = [];
 let lastEditErrorTime = 0;
-const ERROR_INTERVAL_MS = 500;
+const ERROR_INTERVAL_MS = 50;
 let myUsername: string | null = null;
 let liveshare: vsls.LiveShare | null = null;
 let myNotifier: vsls.SharedService | vsls.SharedServiceProxy | null = null;
@@ -29,6 +31,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
     type NotifyHandler<T = any> = (data: T) => void;
 
+    // Utility for host to "watch" their own events
     function makeNamedLocalEvent() {
         const handlers: Record<string, NotifyHandler[]> = {};
 
@@ -55,8 +58,13 @@ export async function activate(context: vscode.ExtensionContext) {
         return myUsername === host;
     }
 
+    function getMyId() {
+        return liveshare?.session.peerNumber;
+    }
+
     const localEvents = makeNamedLocalEvent();
 
+    // General function to send a mass notif
     async function sendMassNotif(name: string, data: any) {
         if (isHost()) {
             // Am host -- if not request to send to all (this shouldn't happen), send to everyone
@@ -66,7 +74,6 @@ export async function activate(context: vscode.ExtensionContext) {
             }
 
             // Do function myself when host
-
             console.log("As host, self-notifying %s.", name);
             localEvents.notify(name, data);
         } else {
@@ -76,6 +83,7 @@ export async function activate(context: vscode.ExtensionContext) {
         }
     }
 
+    // General function to watch a mass notif
     async function watchMassNotif(name: string, handler: vsls.NotifyHandler) {
         if (isHost()) {
             // Help a guest broadcast to all, and do the function myself
@@ -98,6 +106,20 @@ export async function activate(context: vscode.ExtensionContext) {
         }
     }
 
+    // Leave the session by resetting all variables.
+    function leave() {
+        inSession = false;
+        host = null;
+        editor = null;
+        users = [];
+        idToUsername = new Map<number, string>();
+        requests = [];
+        lastEditErrorTime = 0;
+        myUsername = null;
+        liveshare = null;
+        myNotifier = null;
+    }
+
     // Initialize notifications that anyone can send and everyone will receive
     function initMassNotifs() {
         // Call all of the watchMassNotifs with the corresponding actions
@@ -109,7 +131,7 @@ export async function activate(context: vscode.ExtensionContext) {
         });
 
         watchMassNotif('detectChange', (data: any) => {
-            if (data.perpetratorID === liveshare?.session.peerNumber && myUsername !== editor) {
+            if (data.perpetratorID === getMyId() && myUsername !== editor) {
                 // It was me and I am not the editor
 
                 const now = Date.now();
@@ -120,15 +142,36 @@ export async function activate(context: vscode.ExtensionContext) {
                     }, 0);
                 }
 
-                vscode.commands.executeCommand('workbench.action.files.revert');
+                // Unfortunately this doesn't quite work as intended, possibly we could tell host to save a copy every edit and revert
+                // But this could lead to issues when simultaneously typing, for now we just spam the perpetrator with popups 
+                // vscode.commands.executeCommand('workbench.action.files.revert');
             }
+        });
+
+        watchMassNotif('endSession', (data: any) => {
+            leave();
+            vscode.window.showInformationMessage(`The OneType session has been ended by the host.`, { modal: true });
+        });
+
+        // Watch when someone leaves
+        watchMassNotif('leave', (data: any) => {
+            if (data.user === editor) {
+                // If the user who left was editing, transfer edit permission to the host.
+                editor = host;
+            }
+
+            users = users.filter(x => x !== data.user);
+            idToUsername.delete( data.id );
+            vscode.window.showInformationMessage(`✅ ${data.user} left.`, { modal: true });
         });
     }
 
-    // The API has a bug where even if you don't edit it will still show that you edited the workspace
+    // The API has a bug where even if you don't edit, it will still show that you edited the workspace
     // But if another person detects that you edited it it's probably your fault
     // So this is just to blame whoever did it and send a message to that person to stop
     // This probably introduces some latency but I have no idea how to do it any other way
+    
+    // Watch for document changes
     vscode.workspace.onDidChangeTextDocument(async event => {
         if (!inSession) {
             return;
@@ -136,9 +179,9 @@ export async function activate(context: vscode.ExtensionContext) {
 
         const editorInstance = vscode.window.activeTextEditor;
         const peer = await liveshare?.getPeerForTextDocumentChangeEvent(event);
-        console.log("Change detected: My peer number is " + liveshare?.session.peerNumber + " and the peer responsible is " + peer?.peerNumber + ".");
+        console.log("Change detected: My peer number is " + getMyId() + " and the peer responsible is " + peer?.peerNumber + ".");
         
-        if (peer?.peerNumber === liveshare?.session.peerNumber) {
+        if (peer?.peerNumber === getMyId()) {
             // This was me, ignore 
             return;
         }
@@ -147,7 +190,7 @@ export async function activate(context: vscode.ExtensionContext) {
         sendMassNotif('detectChange', { perpetratorID: peer?.peerNumber });
     });
 
-
+    // Host a session as the host of the Live Share session.
     context.subscriptions.push(vscode.commands.registerCommand('onetype.hostSession', async () => {
         if (inSession) {
             vscode.window.showErrorMessage('Already in a session.');
@@ -156,7 +199,12 @@ export async function activate(context: vscode.ExtensionContext) {
 
         liveshare = await vsls.getApi();
         if (!liveshare) {
-            vscode.window.showErrorMessage('LiveShare not detected.');
+            vscode.window.showErrorMessage('Live Share not detected.');
+            return;
+        }
+
+        if (liveshare?.session.role !== vsls.Role.Host) {
+            vscode.window.showErrorMessage('You must be the host of the Live Share session to host the OneType session.');
             return;
         }
 
@@ -179,14 +227,25 @@ export async function activate(context: vscode.ExtensionContext) {
         users = [username];
         requests = [];
 
+        // Watch for joins to broadcast to guests.
         service.onNotify('join', async (data: any) => {
             console.log("Received join notification of %s as host.", data.username);
             if (!users.includes(data.username)) {
                 users.push(data.username);
                 debugSessionState();
-                await service.notify('initiateJoin', { host, editor, users, requests });
+                await service.notify('initiateJoin', { host, editor, users, idToUsername, requests });
                 vscode.window.showInformationMessage("✅ User " + data.username + " joined.");
                 console.log("Sent initiateJoin to all users.");
+            }
+        });
+
+        // Watch for leaves (only by Live Share) to broadcast to guests.
+        liveshare.onDidChangePeers( async (e) => {
+            for (const peer of e.removed) {
+                const username = idToUsername.get(peer?.peerNumber);
+                console.log(`Detected ${username} leave via Live Share.`);
+
+                await sendMassNotif('leave', { username: username, id: peer?.peerNumber });
             }
         });
 
@@ -196,11 +255,12 @@ export async function activate(context: vscode.ExtensionContext) {
 
         initMassNotifs();
 
-        vscode.window.showInformationMessage('✅ OneType session started. Share your LiveShare link to others to join the session.');
+        vscode.window.showInformationMessage('✅ OneType session started. Share your Live Share link to others to join the session.');
         console.log("Hosting started.");
         debugSessionState();
     }));
 
+    // Join a session as a guest of the Live Share session.
     context.subscriptions.push(vscode.commands.registerCommand('onetype.joinSession', async () => {
         if (inSession) {
             vscode.window.showErrorMessage('Already in a session.');
@@ -209,11 +269,16 @@ export async function activate(context: vscode.ExtensionContext) {
 
         liveshare = await vsls.getApi();
         if (!liveshare) {
-            vscode.window.showErrorMessage('LiveShare not detected.');
+            vscode.window.showErrorMessage('Live Share not detected.');
             return;
         }
 
-        const username = await vscode.window.showInputBox({ prompt: 'Enter your username' });
+        if (liveshare?.session.role !== vsls.Role.Guest) {
+            vscode.window.showErrorMessage('You must be a guest of the Live Share session to join the OneType session.');
+            return;
+        }
+
+        const username = await vscode.window.showInputBox({ prompt: 'Enter your username:' });
         if (!username) {
             return;
         }
@@ -228,13 +293,14 @@ export async function activate(context: vscode.ExtensionContext) {
 
         myNotifier = proxy;
 
-        await proxy.notify('join', { username });
+        await proxy.notify('join', { username: username, peerNumber: getMyId() });
         console.log("Sent join notification as guest.");
 
         proxy.onNotify('initiateJoin', (data: any) => {
             console.log("Received initiateJoin as guest.");
+
             let updUsers;
-            ({ host, editor, users: updUsers, requests } = data);
+            ({ host, editor, users: updUsers, idToUsername, requests } = data);
             const newUsers = updUsers.filter((x: string) => !users.includes(x));
 
             if (newUsers.length === 1) {
@@ -252,6 +318,19 @@ export async function activate(context: vscode.ExtensionContext) {
         initMassNotifs();
     }));
 
+    // As host, end the session for all users.
+    context.subscriptions.push(vscode.commands.registerCommand('onetype.endSession', async () => {
+        await sendMassNotif('endSession', {});
+    }));
+
+    // As guest, leave the session. Need to take care of directly exiting Live Share separately.
+    context.subscriptions.push(vscode.commands.registerCommand('onetype.leave', async () => {
+        await sendMassNotif('leave', { user: myUsername, id: getMyId() });
+        leave();
+        vscode.window.showInformationMessage('✅ Left the OneType session.'); 
+    }));
+
+    // As editor, give access to another individual.
     context.subscriptions.push(vscode.commands.registerCommand('onetype.giveAccess', async () => {
         if (!inSession || myUsername !== editor) {
             vscode.window.showErrorMessage('Only the current editor can give access.');
